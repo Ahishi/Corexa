76d8bc4aadf984fe248229f793c692acbcee9e53
Kalle <kalle.voutilainen@metropolia.fi>
2020-09-29 13:03:01 +0300

    Asetuksiin tehty verensokeriyksikön valinta spinner.

--- app/src/main/java/com/example/corexa/SettingsActivity.java
+++ app/src/main/java/com/example/corexa/SettingsActivity.java
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //Verensokeriyksikön spinneri
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.yksikot, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //
+
 
         bottomNavigationView = findViewById(R.id.bottomNav);
 
