8505524229d84f2fd787dee57ddb9e5fcb842630
kallevo <kalle.voutilainen@metropolia.fi>
2020-10-04 16:56:15 +0300

    Muokkaus etusivun valmis nappiin

--- app/src/main/java/com/example/corexa/MainActivity.java
+++ app/src/main/java/com/example/corexa/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.corexa.history.HistoryActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
         NavbarView nav = new NavbarView(this, R.id.home);
         nav.navbar();
 
+        button = (Button) findViewById(R.id.Valmis);
+        button.setOnClickListener(this);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                button.setText("Tehty");
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        button.setText("Tehty");
     }
 }
