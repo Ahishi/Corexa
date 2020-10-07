04f36b6169a274ac846fb7438acf8ea70e250d50
fullstack-aritenhunen <arikapteeni@gmail.com>
2020-10-07 19:26:30 +0300

    Asetuksista voi nollata historian tiedot.

--- app/src/main/java/com/example/corexa/SettingsActivity2.java
+++ app/src/main/java/com/example/corexa/SettingsActivity2.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.corexa.history.Historyglobal;
 
 public class SettingsActivity2 extends AppCompatActivity {
 
@@ -23,6 +28,19 @@ public class SettingsActivity2 extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
+
+        Button poisto = findViewById(R.id.poisto);
+        poisto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences preferences = getSharedPreferences("history", 0);
+                preferences.edit().remove("arvot").commit();
+                Historyglobal.getInstance().getHistorylistValues().clear();
+                Toast toast1 = Toast.makeText(SettingsActivity2.this,"Tiedot poistettu.",Toast.LENGTH_LONG);
+                toast1.show();
+            }
+        });
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
